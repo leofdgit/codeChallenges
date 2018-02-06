47c034a1c348f95d6a159f452c72891379a87e5c
leofdgit <33867485+leofdgit@users.noreply.github.com>
2018-02-06 12:39:57 +0000

    added a second, more efficient path search

--- Python/Graph_Theory/breadth_first.py
+++ Python/Graph_Theory/breadth_first.py
@@ -47,6 +47,49 @@ def bfs_p(ug,start,end):
 
     return paths
 
+  
+  def get_node(queue):
+    path = queue.pop(0)
+    if type(path) == int:
+        node = path
+        path = [path]
+    else:
+        node = path[-1]
+    return path, node
+
+
+def is_intersection(G,node,path,seen_paths, seen_nodes, queue):
+    for n in set(G.neighbours[node]) - set(path):
+        if n in seen_nodes:
+            picker = [i for i in seen_paths if i[-1] == n][0][::-1]
+            for i in picker:
+                path += [i]
+            return path
+        seen_paths.append(path + [n])
+        seen_nodes[n]
+        queue.append(path + [n])
+    return False
+
+def bfs_p2(ug,start,end):
+    '''
+    Like bfs except this finds unique paths from the start node to the end node.
+    This implementation starts bfs from start and end, and returns when two paths intersect.
+    '''
+    queue_s = [start]
+    queue_e = [end]
+    all_visits = defaultdict(int)
+    all_paths = []
+    while queue_s and queue_e:
+        path_s, node_s = get_node(queue_s)
+        path_e, node_e = get_node(queue_e)
+        s_intersect = is_intersection(ug, node_s, path_s, all_paths, all_visits, queue_s)
+        if s_intersect:
+            return s_intersect
+        e_intersect = is_intersection(ug, node_e, path_e, all_paths, all_visits, queue_e)
+        if e_intersect:
+            return e_intersect
+
+    return -1
 
 '''
 #test
